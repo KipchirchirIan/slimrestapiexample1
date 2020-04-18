ce59e94abe28a591c9f6bfc4b2c7ca272c9eaeb0
KipchirchirIan <potterke4@gmail.com>
2020-04-18 22:25:49 +0300

    Added route for creating new art

--- src/routes/art.php
+++ src/routes/art.php
@@ -42,4 +42,49 @@ return function (App $app) {
 
         return $response->withHeader('Content-Type', 'application/json');
     });
+
+    /**
+     * Add new Art data
+     */
+    // Create HTTP POST request
+    $app->post('/api/v1/art/add', function (Request $request, Response $response) {
+        // get the parameters from the form submit
+        $form_data =$request->getParsedBody();
+
+        $title = $form_data['title'];
+        $date = $form_data['date'];
+        $technique = $form_data['technique'];
+        $url = $form_data['url'];
+
+        try {
+            // Get DB object
+            $db = new Db();
+
+            // Connect to db
+            $db = $db->connect();
+
+            $sql = "INSERT INTO ART(TITLE, DATE, TECHNIQUE, URL) VALUES (:title, :date, :technique, :url)";
+
+            $stmt = $db->prepare($sql);
+
+            // bind each parameter
+            $stmt->bindParam(':title', $title);
+            $stmt->bindParam(':date', $date);
+            $stmt->bindParam(':technique', $technique);
+            $stmt->bindParam(':url', $url);
+
+            // execute SQL
+            $stmt->execute();
+            $payload = '{ "notice": { "msg": "New Art Added." } }';
+
+            $response->getBody()->write($payload);
+
+        } catch (PDOException $e) {
+            // show error message as Json format
+            $payload = '{"error": {"msg": ' . $e->getMessage() . '} }';
+            $response->getBody()->write($payload);
+        }
+
+        return $response;
+    });
 };
