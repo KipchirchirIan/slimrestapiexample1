9bd00e9e5de5a6172c7f1c33b4514c3d462207b9
KipchirchirIan <potterke4@gmail.com>
2020-04-19 01:28:56 +0300

    Added configs for DB connection using environment variables

---
+++ src/config/config.php
@@ -0,0 +1,12 @@
+<?php
+// MySQL Hostname
+define('DB_HOST', getenv('DB_HOST'));
+
+// MySQL database username
+define('DB_USER', getenv('DB_USER'));
+
+// MySQL password for user
+define('DB_PASS', getenv('DB_PASS'));
+
+// The name of the database
+define('DB_NAME', getenv('DB_NAME'));
